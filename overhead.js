84f8a2f560597b931b40dd63d51ae43a876d003b
Paulo Lima <paulobruny@gmail.com>
2019-10-18 22:27:35 -0300

    overhead calculator

--- overhead.js
+++ overhead.js
@@ -2,15 +2,26 @@ let startTime = 0;
 let endTime = 0;
 accumulator = 0;
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 10000; i++) {
     startTime = process.hrtime();
     for (let j = 0; j < 1000000; j++) {
 
     }
     endTime = process.hrtime(startTime);
-    accumulator += parseFloat(endTime[1] / 1000000);
-    console.log("Overhead execution time: %d", endTime[1] / 1000000);
+    accumulator += (parseFloat(endTime[1] / 1000000));
 }
 
-console.log("mean: %d", (accumulator / 20));
+const mean = (values, population) => {
+  let mean = values.reduce((total, arr) => {
+    return total + arr;
+  })
+
+  return parseFloat(mean / population);
+}
+
+// accumulator.splice(0, 3);
+// console.log(accumulator);
+// console.log(mean(accumulator, 22));
+
+console.log("mean: %d", (accumulator / 10000));
 
